7c65ab72be40d02b33c502a3a343adf4964be48f
drinkintiger <tsoy_vladimir@yahoo.com>
2013-11-11 11:14:14 -0600

    Initial commit for the tutorial. Back-end is done.

---
+++ pizzastore/src/test/java/com/tsoy/pizza/domain/BaseDataOnDemand.java
@@ -0,0 +1,6 @@
+package com.tsoy.pizza.domain;
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = Base.class)
+public class BaseDataOnDemand {
+}
---
+++ pizzastore/src/test/java/com/tsoy/pizza/domain/PizzaOrderDataOnDemand.java
@@ -0,0 +1,6 @@
+package com.tsoy.pizza.domain;
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = PizzaOrder.class)
+public class PizzaOrderDataOnDemand {
+}
